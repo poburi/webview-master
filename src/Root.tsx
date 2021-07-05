5ba77ade62ab61493516dc1b17b48d844c093e31
poburi <shohom35@naver.com>
2021-07-05 22:49:35 +0900

    Prevented Backhandler exit on Android

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { View, Text, TouchableWithoutFeedback } from 'react-native';
-import styled from 'styled-components/native';
+import WebView from 'react-native-webview';
+import { BackHandler } from 'react-native';
 
 const Root = () => {
-  return (
-    <>
-      <View>
-        <Text>안늉</Text>
-      </View>
-    </>
-  );
+  const webviewRef = useRef<WebView>(null);
+  const onAndroidBackPress = (): boolean => {
+    if (webviewRef.current) {
+      webviewRef.current.goBack();
+      return true; // prevent default behavior (exit app)
+    }
+    return false;
+  };
+  useEffect(() => {
+    BackHandler.addEventListener('hardwareBackPress', onAndroidBackPress);
+    return () => BackHandler.removeEventListener('hardwareBackPress', onAndroidBackPress);
+  }, []);
+  return <WebView ref={webviewRef} source={{ uri: 'http://poburi.github.io/' }} />;
 };
 
 export default Root;
 
-const ModalButton = styled(Text)``;
+const styles = {
+  container: {
+    flex: 1,
+    backgroundColor: 'pink',
+  },
+};
