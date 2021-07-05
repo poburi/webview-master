80200eed67c50147863e58f3db88f76771c90a9f
poburi <shohom35@naver.com>
2021-07-05 23:21:39 +0900

    Added exit app

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,23 +1,37 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { View, Text, TouchableWithoutFeedback } from 'react-native';
 import WebView from 'react-native-webview';
 import { BackHandler } from 'react-native';
+import RNExitApp from 'react-native-exit-app';
 
 const Root = () => {
   const webviewRef = useRef<WebView>(null);
-  const onAndroidBackPress = (): boolean => {
-    if (webviewRef.current) {
+  const [canGoBack, setCanGoBack] = useState(false);
+
+  const onAndroidBackPress = () => {
+    if (webviewRef.current && canGoBack) {
       webviewRef.current.goBack();
       return true; // prevent default behavior (exit app)
     }
+
     return false;
   };
+
   useEffect(() => {
     BackHandler.addEventListener('hardwareBackPress', onAndroidBackPress);
     return () => BackHandler.removeEventListener('hardwareBackPress', onAndroidBackPress);
   }, []);
-  return <WebView ref={webviewRef} source={{ uri: 'http://poburi.github.io/' }} />;
+
+  return (
+    <WebView
+      ref={webviewRef}
+      source={{ uri: 'http://poburi.github.io/' }}
+      onNavigationStateChange={(state) => {
+        setCanGoBack(state.canGoBack);
+      }}
+    />
+  );
 };
 
 export default Root;
